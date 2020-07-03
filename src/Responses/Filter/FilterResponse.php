5f2840c7926e6d573c8406d5797b2a202f34104c
canberkmono <canberk.gecgel@monodijital.com>
2020-07-03 09:57:05 +0300

    project exception namespace updated

--- src/Responses/Filter/FilterResponse.php
+++ src/Responses/Filter/FilterResponse.php
@@ -1,1 +1,13 @@
 <?php
+
+namespace App\Responses\Filter;
+
+class FilterResponse
+{
+    public function response()
+    {
+        return [
+            "image" => "http://image.com"
+        ];
+    }
+}
